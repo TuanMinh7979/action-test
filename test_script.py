792790adba5e727f1610ee91f910327a17a84db2
tuant4 <tuant4@fpt.com>
2026-06-25 06:46:40 +0700

    aaa

--- test_script.py
+++ test_script.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello tu GitHub Actions!")
-    print("File nay co ham main() nen duoc tu dong chay.")
+    print("File nay co ham main() nen duoc tu dong chay123.")
 
 
 if __name__ == "__main__":
